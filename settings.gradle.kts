32593c8ef1de05f339f2805d32274c3e5de41413
Marc ROZANC <marc.git.dev@mro.slmail.me>
2023-06-03 22:23:40 +0200

    Enable Gradle remote build cache

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,21 @@ pluginManagement {
     }
 }
 
+buildCache {
+    val isCiServer = System.getenv().containsKey("CI")
+    System.getenv("GRADLE_CACHE_URL")?.let { cacheUrl ->
+        remote<HttpBuildCache> {
+            url = uri(cacheUrl)
+            isPush = isCiServer
+            System.getenv("GRADLE_CACHE_USER")?.let { cacheUser ->
+                credentials {
+                    username = cacheUser
+                    password = System.getenv("GRADLE_CACHE_PASSWORD")
+                }
+            }
+        }
+    }
+}
 
 rootProject.name = "gradle-helm-plugin-parent"
 
